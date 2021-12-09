e0f0d699300b2a0bc878450b3eca3575ff449c8e
Maduki-tech <maduki.business@gmail.com>
2021-12-09 21:40:30 +0100

    testing

--- screens/LedAdd.tsx
+++ screens/LedAdd.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
 export default function LedAdd() {
-    return (
-        <View>
-            <Text>LedAdd</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>David Teste Github</Text>
+    </View>
+  );
 }
